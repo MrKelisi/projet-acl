7552cba60f373f2112b0ff3b65e91e67b0c66b76
Florian Roméo <florian.romeo@feragon.net>
2018-12-11 17:21:42 +0100

    Collaboration - Sauvegarde

--- src/modele/highscores/TableauRecords.java
+++ src/modele/highscores/TableauRecords.java
@@ -1,9 +1,11 @@
 package modele.highscores;
 
+import modele.Joueur;
 import modele.belote.JoueurActif;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class TableauRecords implements Iterable<Record> {
     private static final int MAX_SCORES = 10;
@@ -30,28 +32,46 @@ public class TableauRecords implements Iterable<Record> {
         return instance;
     }
 
+    public Record findRecordOf(Joueur joueur) {
+        for(Record record : records) {
+            if(record.getJoueur().equals(joueur)) {
+                return record;
+            }
+        }
+
+        throw new NoSuchElementException("Le joueur n'a pas été trouvé");
+    }
+
+    /**
+     * Retourne l'indice auquel devra être placé le record
+     * @param score du joueur
+     * @return Indice
+     */
+    private int getIndex(int score) {
+        int index = 0;
+
+        for(Record record : records) {
+            if(record.getScore() <= score) {
+                index++;
+            }
+        }
+
+        return index;
+    }
+
     /**
      * Ajoute un nouveau record au tableau des records à la date d'exécution
      * @param joueurActif JoueurActif
      */
     public void ajouter(JoueurActif joueurActif) {
+        try {
+            records.remove(findRecordOf(joueurActif));
+        }
+        catch (NoSuchElementException e) {
 
-        int index = 0;
-
-        for(Record record : records) {
-            if(record.getJoueur().equals(joueurActif)) {
-                if(joueurActif.score() >= record.getScore()) {
-                    return;
-                }
-                records.remove(record);
-                break;
-            }
-            if(record.getScore() <= joueurActif.score()) {
-                index++;
-            }
         }
 
-        records.add(index, new Record(joueurActif, joueurActif.score()));
+        records.add(getIndex(joueurActif.score()), new Record(joueurActif, joueurActif.score()));
 
         if(records.size() > MAX_SCORES) {
             records.remove(records.size() - 1);
