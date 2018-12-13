714d9f81cf681ddd7c455edf9d78f534bc568ece
Florian Roméo <florian.romeo@feragon.net>
2018-12-13 11:53:05 +0100

    UML classes modèle

--- src/modele/belote/JeuBelote.java
+++ src/modele/belote/JeuBelote.java
@@ -62,14 +62,23 @@ public class JeuBelote extends Observable {
         notifyObservers(this);
     }
 
+    /**
+     * @return Première carte tirée
+     */
     public CarteAJouer getCarte1() {
         return carte1;
     }
 
+    /**
+     * @return Seconde carte tirée
+     */
     public CarteAJouer getCarte2() {
         return carte2;
     }
 
+    /**
+     * @return Résultat du tirage
+     */
     public int getResultat() {
         return resultat;
     }
